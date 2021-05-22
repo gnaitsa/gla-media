532b30a5c51d0402e55b1d577495d9105ce76a4c
nait saada ghenima <ghenima.nait-saada@etu.u-pec.fr>
2021-05-22 02:48:36 +0200

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,52 @@
+<?php /*require('sharedFiles/pageStart.inc.php');*/ ?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="a">
+        <title> Médiathèque GTA en ligne </title>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+		<?php /* include('sharedFiles/style.inc.php'); 
+		 include('sharedFiles/javascript.inc.php'); */?>
+    
+	</head>
+	<body>
+   
+  <nav>
+		<ul id="nav">
+			    <li><a href="index.php"> Accueil</a></li>
+					<li><a href="Aporopos.php">À propos de la médiathèque</a></li>
+					<li><a href="consulter_catalogue.php">consulter le catalogue</a></li>
+					<li><a href="aboutOpenHours.php">help</a></li>
+				
+    </ul>	
+    
+  </nav>
+  <div class = bouton_connexion ><a href="aboutRules.php">Se connecter</a></div>
+
+        <?php /*require('sharedfile/header.inc.php'); */?>
+        <div class = "title">
+            <h1>Médiathèque GTA</h1>
+        </div>
+        <div class="box">
+				<h3> Bienvenue dans l’espace en ligne de la médiathèque GTA , ceci est un espace de partage et d’échange en ligne ,vous pouvez a tout moment consulter les différents documents que vous proposent la médiathèque à savoir des livres ,des CD ainsi que des DVD ,dans différents genres.
+        Si un document vous intéresse et que vous souhaitez l’emprunter, c’est simple ! commencez tout d’abord par vous inscrire à la médiathèque en suivant le lien se connecter sur votre gauche, de ce fait vous aurez un identifiant client qui va vous permettre d’emprunter ou de réserver des documents à tout moment, bien sûr selon les disponibilités.
+        Une fois votre compte crée, vous pouvez vous connecter à tout moment et effectuer des emprunts ou bien des réservations.
+        Pensez à consulter plus souvent  notre site, afin de découvrir les nouveautés :)
+        Bonne visite :)</h3>
+</div>
+      <!--<p>Travail fait par Alexandre , Thibault , Ghenima</p></br>
+
+      <p>Encadreurs : </p>
+      <nav>
+        <ul>
+          <li><a>Mailys</a></li>
+          
+        </ul>
+        <ul>
+          <li><a >Burkhart Wolff</a></li>
+        </ul>
+      </nav>
+-->
+    	</body>
+</html
